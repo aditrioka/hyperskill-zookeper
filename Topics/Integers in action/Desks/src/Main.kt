464666c4c7f9d100bbd9e6309e4c96178c4a472b
Adi Trioka <aditrioka@gmail.com>
2024-12-27 09:33:18 +0700

    First commit

---
+++ Topics/Integers in action/Desks/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numOfStudents1 = readln().toInt()
+    val numOfStudents2= readln().toInt()
+    val numOfStudents3 = readln().toInt()
+
+    val result = ((numOfStudents1 + 1) / 2) + ((numOfStudents2 + 1) / 2) + ((numOfStudents3 + 1) / 2)
+    println(result)
+}
---
+++ Topics/Loops  repeat statement/Sum of squares of even numbers up to a limit/src/Main.kt
@@ -0,0 +1,27 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    // Create a Scanner object for taking input
+    val reader = Scanner(System.`in`)
+
+    // Read an integer input
+    val limit = reader.nextInt()
+
+    // Check if limit is less than 0
+    if (limit < 0) {
+        println("Invalid Input")
+    } else {
+        // Write a loop here to calculate the sum of squares of even numbers up to the limit
+        // Use the 'repeat' construct
+        // After calculating the sum, print it
+        var sumOfSquares = 0
+        var currentNumber = 1
+        repeat(limit) {
+            if (currentNumber % 2 == 0) {
+                sumOfSquares += (currentNumber * currentNumber)
+            }
+            currentNumber++
+        }
+        println(sumOfSquares)
+    }
+}
